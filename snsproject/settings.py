a9e380da3d5d0110605eeb52a732e4f859a6fe54
kitazawahibiki <ktzwhbk56@gmail.com>
2025-04-13 20:43:13 +0900

    settings.py変更

--- snsproject/settings.py
+++ snsproject/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "sns-app-t7vs.onrender.com"]
 
 
 # Application definition
